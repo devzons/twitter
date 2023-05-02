c66c4643f840a04f1d6abe1a5b43bb08bb991e5b
Don Yu <37088038+devzons@users.noreply.github.com>
2023-05-02 12:32:29 -0700

    send data to firebase and add loading style

--- components/Input.jsx
+++ components/Input.jsx
@@ -1,10 +1,69 @@
-import { PhotoIcon, FaceSmileIcon } from '@heroicons/react/24/outline'
+import { useState, useRef } from 'react'
+import {
+  addDoc,
+  collection,
+  doc,
+  updateDoc,
+  serverTimestamp,
+} from 'firebase/firestore'
 import { useSession, signOut } from 'next-auth/react'
+import {
+  PhotoIcon,
+  FaceSmileIcon,
+  XMarkIcon,
+} from '@heroicons/react/24/outline'
+import { getDownloadURL, ref, uploadString } from 'firebase/storage'
+
+import { db, storage } from '../firebase'
 
 export default function Input() {
   const { data: session } = useSession()
+  const [input, setInput] = useState('')
+  const [selectedFile, setSelectedFile] = useState(null)
+  const [loading, setLoading] = useState(false)
+  const filePickerRef = useRef(null)
 
-  console.log(session)
+  const sendPost = async (e) => {
+    e.preventDefault()
+    if (loading) return
+    setLoading(true)
+
+    const docRef = await addDoc(collection(db, 'posts'), {
+      id: session.user.uid,
+      text: input,
+      userImg: session.user.image,
+      timestamp: serverTimestamp(),
+      name: session.user.name,
+      username: session.user.username,
+    })
+
+    const imageRef = ref(storage, `posts/${docRef.id}/image`)
+
+    if (selectedFile) {
+      await uploadString(imageRef, selectedFile, 'data_url').then(async () => {
+        const downloadURL = await getDownloadURL(imageRef)
+        await updateDoc(doc(db, 'posts', docRef.id), {
+          image: downloadURL,
+        })
+      })
+    }
+
+    setInput('')
+    setSelectedFile(null)
+    setLoading(false)
+  }
+
+  const addImageToPost = (e) => {
+    e.preventDefault()
+    const reader = new FileReader()
+    if (e.target.files[0]) {
+      reader.readAsDataURL(e.target.files[0])
+    }
+
+    reader.onloadend = (readerEvent) => {
+      setSelectedFile(readerEvent.target.result)
+    }
+  }
 
   return (
     <>
@@ -23,16 +82,50 @@ export default function Input() {
                 className='w-full border-none focus:ring-0 text-lg placeholder-gray-700 tacking-wide min-h-[50px] text-gray-700'
                 rows='2'
                 placeholder="What's happening?"
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
               />
+              {selectedFile && (
+                <div className='relative'>
+                  <XMarkIcon
+                    className='h-6 absolute cursor-pointer bg-white rounded-full hover:bg-black hover:text-white'
+                    onClick={() => setSelectedFile(null)}
+                  />
+                  <img
+                    src={selectedFile}
+                    alt='image'
+                    className={`${loading && 'animate-pulse'}`}
+                  />
+                </div>
+              )}
             </div>
             <div className='flex items-center justify-between pt-2.5'>
-              <div className='flex'>
-                <PhotoIcon className='h-10 w-10 hoverEffect p-2 text-sky-500 hover:bg-sky-100' />
-                <FaceSmileIcon className='h-10 w-10 hoverEffect p-2 text-sky-500 hover:bg-sky-100' />
-              </div>
-              <button className='bg-blue-400 text-white px-4 py-1.5 rounded-full font-bold shadow-md hover:brightness-95 disabled:opacity-50'>
-                Tweet
-              </button>
+              {!loading && (
+                <>
+                  <div className='flex'>
+                    <div
+                      className=''
+                      onClick={() => filePickerRef.current.click()}
+                    >
+                      <PhotoIcon className='h-10 w-10 hoverEffect p-2 text-sky-500 hover:bg-sky-100' />
+                      <input
+                        type='file'
+                        hidden
+                        ref={filePickerRef}
+                        onChange={addImageToPost}
+                      />
+                    </div>
+                    <FaceSmileIcon className='h-10 w-10 hoverEffect p-2 text-sky-500 hover:bg-sky-100' />
+                  </div>
+                  <button
+                    onClick={sendPost}
+                    disabled={!input.trim()}
+                    className='bg-blue-400 text-white px-4 py-1.5 rounded-full font-bold shadow-md hover:brightness-95 disabled:opacity-50'
+                  >
+                    Tweet
+                  </button>
+                </>
+              )}
             </div>
           </div>
         </div>
